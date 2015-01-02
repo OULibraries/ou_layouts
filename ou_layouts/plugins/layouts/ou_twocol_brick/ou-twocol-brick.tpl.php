58f30ad94bea1cbdb19bb696dc351a7e6ea0ac65
camp5456 <tcamp@ou.edu>
2015-01-02 12:51:34 -0600

    Added Middle Row for Resources Page
    Resources page needed a middle two column responsive row

--- ou_layouts/plugins/layouts/ou_twocol_brick/ou-twocol-brick.tpl.php
+++ ou_layouts/plugins/layouts/ou_twocol_brick/ou-twocol-brick.tpl.php
@@ -12,7 +12,15 @@
 			<?php print $content['tright']; ?>
 		</div>
 	  </div>
-	   
+	    <div class="row">
+		<div class="col-md-6">
+			<?php print $content['mleft']; ?>
+		</div>
+		
+		<div class="col-md-6">
+			<?php print $content['mright']; ?>
+		</div>	
+	   </div>
 	   <div class="row">
 		<div class="col-md-6">
 			<?php print $content['bleft']; ?>
