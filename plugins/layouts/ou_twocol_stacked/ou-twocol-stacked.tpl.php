abd0a6e3492fc378dbce890ee86eef05bf5d44c6
camp5456 <tcamp@ou.edu>
2015-03-20 16:53:42 -0500

    fix 2-column.

--- plugins/layouts/ou_twocol_stacked/ou-twocol-stacked.tpl.php
+++ plugins/layouts/ou_twocol_stacked/ou-twocol-stacked.tpl.php
@@ -1,8 +1,10 @@
 <div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
- <div class="container-fluid">
+
  
 <div class="row">
+	<div class="col-md-12">
     <?php print $content['top']; ?>
+	</div>
   </div>
   
   <div class="row">
@@ -15,7 +17,9 @@
   </div>
   
   <div class="row">
+	<div class="col-md-12">
     <?php print $content['bottom']; ?>
+	</div>
   </div>
 </div>
-</div>
+
