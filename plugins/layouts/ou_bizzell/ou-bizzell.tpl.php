eaf9e7821f070e3c7ef80ef7906d4071b197d400
camp5456 <tcamp@ou.edu>
2015-03-20 10:45:39 -0500

    New Template for Bizzell page

---
+++ plugins/layouts/ou_bizzell/ou-bizzell.tpl.php
@@ -0,0 +1,70 @@
+<div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+		<div class="container-fluid">
+		<div class="row location_header">
+			<div class="col-md-8">
+				<?php print $content['header_img']; ?>
+			</div>
+			<div class="col-md-4">
+				<?php print $content['header_side']; ?>
+			</div>
+		</div>
+		
+		<div class="row location_description">
+			<div class="col-md-12">
+				<?php print $content['description']; ?>
+			</div>
+		</div>
+		
+		<div class="row location_top">
+			<div class="col-md-12">
+				<?php print $content['top']; ?>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-md-6">
+				<?php print $content['tleft']; ?>
+			</div>
+			<div class="col-md-6">
+				<?php print $content['tright']; ?>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-md-12">
+				<?php print $content['middle']; ?>
+			</div>
+		</div>
+		
+		<div class="location_middle">
+		<div class="row">
+			<div class="col-md-6">
+				<?php print $content['mleft']; ?>
+			</div>
+		
+			<div class="col-md-6">
+				<?php print $content['mright']; ?>
+			</div>
+			</div>
+		</div>	
+		
+		<div class="row location_bottom">
+			<div class="col-md-12">
+				<?php print $content['bottom']; ?>
+			</div>
+		</div>
+		
+		<div class="row location_footer">
+			<div class="col-md-4">
+				<?php print $content['footerl']; ?>
+			</div>
+			<div class="col-md-4">
+				<?php print $content['footerm']; ?>
+			</div>
+			<div class="col-md-4">
+				<?php print $content['footerr']; ?>
+			</div>
+		</div>
+		 </div>
+	</div>
+
