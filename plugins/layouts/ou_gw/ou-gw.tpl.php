018cd505b488342910f75f57a0d6189ec4890179
camp5456 <tcamp@ou.edu>
2015-05-07 13:30:41 -0500

    Adding template for GW Home

---
+++ plugins/layouts/ou_gw/ou-gw.tpl.php
@@ -0,0 +1,61 @@
+ <!-- used on about and resources page -->
+ <div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+ 
+  <div class="container-fluid">
+  
+	  <div class="row">
+		<div style="margin:0px 15px 0px 15px;">
+		<?php print $content['top']; ?>
+		</div>
+	  </div>
+	  
+	  <div class="row">
+		<div class="col-md-5">
+			<?php print $content['tleft']; ?>
+		</div>
+		<div class="col-md-4">
+			<?php print $content['tmiddle']; ?>
+		</div>
+		<div class="col-md-3">
+			<?php print $content['tright']; ?>
+		</div>
+	  </div>
+	  
+	  <div class="row">
+		<div style="margin:0px 15px 0px 15px;">
+			<?php print $content['middle']; ?>
+		</div>
+	  </div>
+	  
+		<div class="row">
+			<div class="col-md-2">
+				<?php print $content['mdetail']; ?>
+			</div>
+			<div class="col-md-3">
+				<?php print $content['mleft']; ?>
+			</div>
+			<div class="col-md-3">
+				<?php print $content['mmiddle']; ?>
+			</div>
+			<div class="col-md-4">
+				<?php print $content['mright']; ?>
+			</div>
+	   </div>
+	   
+	   <div class="row" style="margin-left:0px;">
+		<div class="col-md-8 col-sm-6">
+			<?php print $content['bleft']; ?>
+		</div>
+		
+		<div class="col-md-4 col-sm-6">
+			<?php print $content['bright']; ?>
+		</div>	
+	  </div>
+	  
+	  <div class="row">
+		<div style="margin:0px 15px 0px 15px;">
+		<?php print $content['bottom']; ?>
+		</div>
+	  </div>
+	</div>
+</div>
