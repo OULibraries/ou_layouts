abaa3b2a592bb8559d74f6ff68e0a4d3ec433032
camp5456 <tcamp@ou.edu>
2015-05-19 12:40:55 -0500

    adding styles to GW layout

--- plugins/layouts/ou_gw/ou-gw.tpl.php
+++ plugins/layouts/ou_gw/ou-gw.tpl.php
@@ -3,20 +3,22 @@
  
   <div class="container-fluid">
   
-	  <div class="row">
-		<div style="margin:0px 15px 0px 15px;">
-		<?php print $content['top']; ?>
+	  <div class="row locations-description" style="margin:0px 0px 0px 0px;">
+		<div class="col-md-8">
+			<?php print $content['top1']; ?>
+		</div>
+		
+		<div class="col-md-4">
+			<?php print $content['top2']; ?>
 		</div>
 	  </div>
 	  
 	  <div class="row">
-		<div class="col-md-5">
+		<div class="col-md-6 homepage-video">
 			<?php print $content['tleft']; ?>
 		</div>
-		<div class="col-md-4">
-			<?php print $content['tmiddle']; ?>
-		</div>
-		<div class="col-md-3">
+		
+		<div class="homepage-login col-md-6">
 			<?php print $content['tright']; ?>
 		</div>
 	  </div>
@@ -28,7 +30,7 @@
 	  </div>
 	  
 		<div class="row">
-			<div class="col-md-2">
+			<div class="col-md-2 hidden-xs hidden-sm">
 				<?php print $content['mdetail']; ?>
 			</div>
 			<div class="col-md-3">
