f07dab751a56986822e7a4944b21ff65ec23abe2
camp5456 <tcamp@ou.edu>
2015-06-03 13:51:23 -0500

    Adding some padding for layouts

--- plugins/layouts/ou_twocol_stacked/ou-twocol-stacked.tpl.php
+++ plugins/layouts/ou_twocol_stacked/ou-twocol-stacked.tpl.php
@@ -3,22 +3,26 @@
  
 <div class="row">
 	<div class="col-md-12">
-    <?php print $content['top']; ?>
+    <div style="margin:0px 0px 0px 0px;">
+	<?php print $content['top']; ?>
+	</div>
 	</div>
   </div>
   
   <div class="row">
-	<div class="col-md-6">
+	<div class="col-md-6" style="margin-left:0px;">
     <?php print $content['left']; ?>
 	</div>	
-	<div class="col-md-6">
+	<div class="col-md-6" style="margin-right:0px;">
     <?php print $content['right']; ?>
 	</div>
   </div>
   
   <div class="row">
+  <div style="margin:0px 0px 0px 0px;">
 	<div class="col-md-12">
     <?php print $content['bottom']; ?>
+	</div>
 	</div>
   </div>
 </div>
