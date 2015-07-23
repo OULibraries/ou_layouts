915601482ad6ab56553bf009b307c51eb70317f9
camp5456 <tcamp@ou.edu>
2015-07-23 13:09:20 -0500

    Re-configuring resources page
    to accommodate more subjects

--- plugins/layouts/ou_resources/ou-resources.tpl.php
+++ plugins/layouts/ou_resources/ou-resources.tpl.php
@@ -1,16 +1,29 @@
 <div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   <div class="container-fluid">
 	  <div class="row">
+		<div class="col-md-8 col-sm-4 col-xs-12">
+			<?php print $content['top1']; ?>
+		</div>
+		
+		<div class="col-md-4 col-sm-4 col-xs-12">
+			<?php print $content['top2']; ?>
+		</div>
+	  </div>
+	  <div class="row">
 		<?php print $content['top']; ?>
 	  </div>
 	  
-	  <div class="row">
-		<div class="col-md-7 col-sm-6 col-xs-12">
-			<?php print $content['main']; ?>
+	  <div class="row resources_by_subject_links">
+		<div class="col-md-4 col-sm-4 col-xs-12">
+			<?php print $content['topl']; ?>
 		</div>
 		
-		<div class="col-md-5 col-sm-6 col-xs-12">
-			<?php print $content['sidebar']; ?>
+		<div class="col-md-4 col-sm-4 col-xs-12">
+			<?php print $content['topm']; ?>
+		</div>
+		
+		<div class="col-md-4 col-sm-4 col-xs-12">
+			<?php print $content['topr']; ?>
 		</div>
 		
 	  </div>
