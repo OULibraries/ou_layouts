5baf1f37dc289fdcf4641de0b6aa160b1653ffa4
Twila Camp <tcamp@ou.edu>
2016-06-16 14:49:40 -0500

    adding new intranet sidebar template

---
+++ plugins/layouts/ou_intranet_sidebar/ou_intranet_sidebar.tpl.php
@@ -0,0 +1,29 @@
+<div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+		<div class="container-fluid">
+		
+		<div class="row">
+			<div class="col-md-12">
+				<?php print $content['top']; ?>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-md-4">
+				<?php print $content['sidebar']; ?>
+			</div>
+		
+			<div class="col-md-8">
+				<?php print $content['body']; ?>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-md-12">
+				<?php print $content['bottom']; ?>
+			</div>
+		</div>
+		
+		
+		 </div>
+	</div>
+
