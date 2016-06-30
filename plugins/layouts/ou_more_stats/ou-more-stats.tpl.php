206be01191a71ca99e96b3d19c762fad21fd96b3
Tim Smith <timsmith@ou.edu>
2016-06-30 12:58:42 -0500

    adding stats layouts and module

---
+++ plugins/layouts/ou_more_stats/ou-more-stats.tpl.php
@@ -0,0 +1,104 @@
+<!-- used on about and resources page -->
+<div class="<?php print $classes ?>" <?php if (!empty($css_id)) {
+  print "id=\"$css_id\"";
+} ?>>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row">
+          <div class="col-md-12">
+            <?php print $content['topP']; ?>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-2">
+            <?php print $content['top1']; ?>
+          </div>
+          <div class="col-md-2">
+            <?php print $content['top2']; ?>
+          </div>
+          <div class="col-md-2">
+            <?php print $content['top3']; ?>
+          </div>
+          <div class="col-md-2">
+            <?php print $content['top4']; ?>
+          </div>
+          <div class="col-md-2">
+            <?php print $content['top5']; ?>
+          </div>
+          <div class="col-md-2">
+            <?php print $content['top6']; ?>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-12">
+              <?php print $content['mid2']; ?>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <?php print $content['mid3']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['mid4']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['mid5']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['mid6']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['mid7']; ?>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-12">
+              <?php print $content['bot2']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['bot3']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['bot4']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['bot5']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['bot6']; ?>
+            </div>
+          </div>
+        </div>
+      </div><
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-12">
+              <?php print $content['foot1']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['foot2']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['foot3']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['foot4']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['foot5']; ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
--- plugins/layouts/ou_stats/ou-stats.tpl.php
+++ plugins/layouts/ou_stats/ou-stats.tpl.php
@@ -5,47 +5,50 @@
 
   <div class="container-fluid ga-stats-group">
     <div class="row">
-      <div class="col-md-9">
-        <div class="row">
+      <div class="col-md-12">
         <div class="col-md-9">
-          <?php print $content['tleft']; ?>
+          <div class="row">
+            <div class="col-md-9">
+              <?php print $content['tleft']; ?>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-9">
+              <?php print $content['tleft2']; ?>
+            </div>
+          </div>
+          <div class="row" id="threeGroup">
+            <div class="col-md-3">
+              <?php print $content['mleft1of3']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['mleft2of3']; ?>
+            </div>
+            <div class="col-md-3">
+              <?php print $content['mleft3of3']; ?>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <?php print $content['bleft1of2']; ?>
+            </div>
+            <div class="col-md-4">
+              <?php print $content['bleft2of2']; ?>
+            </div>
+          </div>
         </div>
-          </div>
-      <div class="row">
-        <div class="col-md-9">
-          <?php print $content['tleft2']; ?>
+ <div class="row">
+        <div class="col-md-3">
+          <?php print $content['tright']; ?>
         </div>
-      </div>
-        <div class="row" id="threeGroup">
-          <div class="col-md-3">
-            <?php print $content['mleft1of3']; ?>
-          </div>
-          <div class="col-md-3">
-            <?php print $content['mleft2of3']; ?>
-          </div>
-          <div class="col-md-3">
-            <?php print $content['mleft3of3']; ?>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4">
-            <?php print $content['bleft1of2']; ?>
-          </div>
-          <div class="col-md-4">
-            <?php print $content['bleft2of2']; ?>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-3">
-        <?php print $content['tright']; ?>
+   </div>
       </div>
     </div>
   </div>
 </div>
 
 <script>
-  function SetTopMargin () {
+  function SetTopMargin() {
     document.getElementById('threeGroup').style.marginleft = "-150px";
   }
   SetTopMargin();
