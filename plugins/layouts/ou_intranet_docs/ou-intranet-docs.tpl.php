80eac5a3c8f10eca648f87dc23f579b65bad7a64
Digital Scraps <allison.campbell.6222@gmail.com>
2016-07-20 21:37:00 -0500

    update docs layout

--- plugins/layouts/ou_intranet_docs/ou-intranet-docs.tpl.php
+++ plugins/layouts/ou_intranet_docs/ou-intranet-docs.tpl.php
@@ -2,48 +2,57 @@
 
 <!--header and nav buttons for page-->
 
-	<header class="title container">
-		<div class="row">
+	<div class="container-fluid">
+		<div class="row title">
 			<div class="col-md-12" id="title">
 				<?php print $content['top']; ?>
-
-					<div class="nav button row">
-						<section class="col-md-offset-1" id="navbutton">
-							<?php print $content['navbar']; ?>
-						</section>
-				</div>
 			</div>
 		</div>
-	</header>
+
+
+	<div class="row search">
+		<section class="col-xs-12 col-sm-4 col-md-4" id="searchbutton1">
+			<?php print $content['search1']; ?>
+		</section>
+
+		<section class="col-xs-12 col-sm-4 col-md-4" id="searchbutton2">
+			<?php print $content['search2']; ?>
+		</section>
+
+		<section class="col-xs-12 col-sm-4 col-md-4" id="searchbutton3">
+			<?php print $content['search3']; ?>
+		</section>
+		</div>
+	</div>
 
 <!--start 2 column layout-->
 
-	<div class="content container">
-		<div class="row">
+	<div class="container-fluid">
+		<div class="row content">
 			<div class="col-md-8">
 				<?php print $content['body']; ?>
 
-				<section class="row" id="search">
-				<div class="col-md-4">
+				<section class="row">
+				<div class="col-xs-12 col-sm-4 col-md-4">
 						<?php print $content['section1']; ?>
 				</div>
 			</section>
 
 			<section class="row">
-				<div class="col-md-12" id="recent">
+				<div class="col-xs-12 col-sm-4 col-md-4">
 						<?php print $content['section2']; ?>
 				</div>
 			</section>
 
 			<section class="row">
-				<div class="col-md-12" id="collections">
+				<div class="col-sx-12 col-sm-4 col-md-4">
 					<?php print $content['section3']; ?>
 				</div>
 			</section>
 
 		</div> <!--end column search, recent, collections-->
 
-		<section class ="col-md-offset-1 col-md-3" id="toolkits">
+		<section class ="col-md-3" id="toolkits">
 			<?php print $content['sidebar']; ?>
 		</section>
 
