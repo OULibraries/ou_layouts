3f560fae929843b4a8dd06d59f7b46e4ca4491f4
Digital Scraps <allison.campbell.6222@gmail.com>
2016-07-21 09:52:52 -0500

    updated css for docs layout

--- plugins/layouts/ou_intranet_docs/ou-intranet-docs.tpl.php
+++ plugins/layouts/ou_intranet_docs/ou-intranet-docs.tpl.php
@@ -57,37 +57,6 @@
 </div> <!-- container 2 column layout-->
 
 <!--footer-->
-	<footer class="container-fluid">
-=======
-<div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-
-<!--header and nav buttons for page-->
-
-	<div class="container-fluid">
-		<div class="row title">
-			<div class="col-md-8 col-lg-8" id="title">
-				<?php print $content['top']; ?>
-			</div>
-		</div>
-
-
-	<div class="row search">
-		<div class="col-md-8 col-lg-8">
-		<section class="col-xs-12 col-sm-4 col-md-4" id="searchbutton1">
-			<?php print $content['search1']; ?>
-		</section>
-
-		<section class="col-xs-12 col-sm-4 col-md-4" id="searchbutton2">
-			<?php print $content['search2']; ?>
-		</section>
-
-		<section class="col-xs-12 col-sm-4 col-md-4" id="searchbutton3">
-			<?php print $content['search3']; ?>
-		</section>
-		</div>
-	</div>
-</div>
-
 <footer class="container-fluid">
 	<section class="col-md-12" id="footer">
 		<?php print $content['bottom']; ?>
