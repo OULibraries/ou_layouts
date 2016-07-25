4d1234c8dca5d98e0165969d267c46b209225d00
Digital Scraps <allison.campbell.6222@gmail.com>
2016-07-25 14:10:46 -0500

    updating layout for docs page

--- plugins/layouts/ou_intranet_docs/ou-intranet-docs.tpl.php
+++ plugins/layouts/ou_intranet_docs/ou-intranet-docs.tpl.php
@@ -1,57 +1,50 @@
 <div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
-<!--header and nav buttons for page-->
+<!--Upload Button at top of Page-->
 
 	<div class="container-fluid">
-		<div class="row title">
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="title">
+		<div class="row">
+			<div class="col-xs-offset-1 col-xs-10 col-sm-offset-0 col-sm-6 col-md-offset-0 col-md-8 col-lg-offset-0 col-lg-8" id="upload">
 				<?php print $content['top']; ?>
 			</div>
 		</div>
-
-
-	<div class="row search">
-		<div class="col-md-8 col-lg-8">
-		<section class="col-xs-12 col-sm-4 col-md-4" id="searchbutton1">
-			<?php print $content['search1']; ?>
-		</section>
-
-		<section class="col-xs-12 col-sm-4 col-md-4" id="searchbutton2">
-			<?php print $content['search2']; ?>
-		</section>
-
-		<section class="col-xs-12 col-sm-4 col-md-4" id="searchbutton3">
-			<?php print $content['search3']; ?>
-		</section>
-		</div>
-	</div>
-</div>
 
 <!--start 2 column layout-->
 
 	<div class="container-fluid">
 		<div class="row content">
-			<section class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
-				<?php print $content['body']; ?>
+			<div class="col-xs-offset-1 col-xs-10 col-sm-offset-0 col-sm-6 col-md-offset-0 col-md-8 col-lg-offset-0 col-lg-8">
 
+				<!--Recently Added View--->
+				<div class="row">
+					<div class="col-md-12" id="recent">
+						<?php print $content['body']; ?>
+					</div>
+				</div>
 
-				<div class="col-xs-12 col-sm-4 col-md-4">
+			<!--Collections-->
+			<div class="row">
+				<div class="col-xs-offset-1 col-xs-10 col-sm-offset-0 col-sm-12 col-md-offset-0 col-md-12 col-lg-offset-0 col-lg-12" id="collections">"
+
+					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4" id="col">
 						<?php print $content['section1']; ?>
-				</div>
+					</div>
 
-				<div class="col-xs-12 col-sm-4 col-md-4">
+					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4" id="col">
 						<?php print $content['section2']; ?>
-				</div>
+					</div>
 
-				<div class="col-sx-12 col-sm-4 col-md-4">
-					<?php print $content['section3']; ?>
-				</div>
+					<div class="col-sx-12 col-sm-4 col-md-4 col-lg-4" id="col">
+						<?php print $content['section3']; ?>
+					</div>
 
-		</section>
+			</div><!--column collections-->
+		</div> <!--row collections-->
+	</div><!--column recent and collections-->
 
-		<section class ="col-xs-12 col-sm-12 col-md-3 col-lg-3" id="toolkits">
+		<div class ="col-xs-offset-1 col-xs-10 col-sm-offset-0 col-sm-12 col-md-offset-1 col-md-3 col-lg-offset-1 col-lg-3" id="toolkits">
 			<?php print $content['sidebar']; ?>
-		</section>
+		</div>
 
 	</div> <!-- row 2 column layout-->
 </div> <!-- container 2 column layout-->
