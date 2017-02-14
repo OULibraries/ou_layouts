82f7393636e0d8758c96c3253954b60d55f224e0
Twila Camp <tcamp@ou.edu>
2017-02-14 15:19:47 -0600

    adding section for units on about page

--- plugins/layouts/ou_about/ou-about.tpl.php
+++ plugins/layouts/ou_about/ou-about.tpl.php
@@ -61,18 +61,9 @@
 		</div>
 	  </div>
 	  
-	  
-	  <div class="col-md-12">
-		<h2>UNITS</h2>
-	  </div>
-	  
 	  <div class="row content-border">
-		<div class="col-md-6">
-			<?php print $content['left2']; ?>
-		</div>
-		
-		<div class="col-md-6">
-			<?php print $content['right2']; ?>
+		<div class="col-md-12">
+			<?php print $content['departments']; ?>
 		</div>
 	  </div>
 	  
