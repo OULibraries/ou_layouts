388893b75564b59c733021e928b7ac4701478f7a
Tim <timsmith@ou.edu>
2018-09-17 14:07:39 -0500

    new renegades layout

---
+++ plugins/layouts/ou_renegades/ou-renegades.tpl.php
@@ -0,0 +1,48 @@
+<div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+ 
+  <div class="container-fluid">
+  
+	  <div class="row content-border">
+		<div class="col-md-3">
+			<?php print $content['top1']; ?>
+		</div>
+		
+		<div class="col-md-9">
+			<?php print $content['top2']; ?>
+		</div>
+	  </div>
+	  
+	  <div class="row content-border">
+		<div class="col-lg-12 col-md-12">
+			<?php print $content['item1']; ?>
+		</div>
+		
+		<div class="col-lg-3 col-md-3">
+			<?php print $content['item2']; ?>
+		</div>
+	  
+		<div class="col-lg-9 col-md-9">
+			<?php print $content['item3']; ?>
+		</div>
+		
+		<div class="col-lg-3 col-md-3">
+			<?php print $content['item4']; ?>
+		</div>
+		
+		<div class="col-lg-9 col-md-9">
+			<?php print $content['item5']; ?>
+		</div>
+		
+		<div class="col-lg-3 col-md-3">
+			<?php print $content['item6']; ?>
+		</div>
+
+		<div class="col-lg-9 col-md-9">
+			<?php print $content['item7']; ?>
+		</div>
+	  </div>
+	  
+	  
+	</div>
+	
+</div>
